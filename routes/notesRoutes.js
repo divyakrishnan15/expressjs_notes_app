47d458254f776f6e1cf7704ea7b696266b6c50e6
divyakrishnan15 <divyabalajee@gmail.com>
2023-07-01 14:26:49 -0400

    add

---
+++ routes/notesRoutes.js
@@ -0,0 +1,13 @@
+var path = require('path')
+const router = require('express').Router()
+// const apiRoutes = require("./apiRoutes");
+
+router.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+})
+
+router.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+module.exports = router;
